146e5d1be363191d399c276cd5f21faf58f3217a
TJ Holowaychuk <tj@vision-media.ca>
2013-04-23 15:14:50 -0700

    add implementation

--- index.js
+++ index.js
@@ -4,11 +4,35 @@
  */
 
 var path = require('path');
+var langs = require('./data');
 var basename = path.basename;
+
+/**
+ * Expose `isCode()`.
+ */
 
 module.exports = isCode;
 
+/**
+ * Check if `path` is a code filename.
+ *
+ * @param {String} path
+ * @return {Boolean|Object}
+ * @api public
+ */
+
 function isCode(path) {
   var base = basename(path);
+
+  for (var i = 0; i < langs.length; i++) {
+    var lang = langs[i];
+    for (var j = 0; j < lang.regexps.length; j++) {
+      var re = lang.regexps[j];
+      if (re.test(base)) {
+        return lang;
+      }
+    }
+  }
+
   return false;
 }
--- test/index.js
+++ test/index.js
@@ -9,7 +9,14 @@ var assert = require('better-assert');
 describe('isCode(path)', function(){
   describe('when true', function(){
     it('should return information about the language', function(){
+      var lang = isCode('foo.yaml');
+      lang.names.should.eql(['yaml']);
 
+      var lang = isCode('foo.yml');
+      lang.names.should.eql(['yaml']);
+
+      var lang = isCode('some/path/to/Makefile');
+      lang.names.should.eql(['make', 'makefile', 'mf', 'bsdmake']);
     })
   })
 
